f4b273ca899e24e19a9538fa0134a8c06beb61a3
Yuliia <kotyuliya24@gmail.com>
2024-03-31 14:24:41 -0400

    task2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,24 @@
 'use strict';
+
+class Storage {
+  #items;
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items -= itemToRemove;
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
