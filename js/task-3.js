8f9973e842601a79c768e7dc87f574eb9847f10c
Yuliia <kotyuliya24@gmail.com>
2024-03-31 15:31:40 -0400

    task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,35 @@
 'use strict';
+
+class StringBuilder {
+  #value = {};
+
+  constructor(value) {
+    this.#value = value;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value.push(str);
+  }
+
+  padStart(str) {
+
+  }
+
+  padBoth(str) {
+
+  }
+
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
